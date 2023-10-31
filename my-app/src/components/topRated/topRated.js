f70d00e53a34b2cac3c93f355fce26751c228a7b
lawlessowl <erokhin-maksim@bk.ru>
2023-10-31 22:25:32 +0300

    feat: added books cards

--- my-app/src/components/topRated/topRated.js
+++ my-app/src/components/topRated/topRated.js
@@ -5,10 +5,22 @@ export const TopRated = () => {
     return (
         <div className={style.boxBackground}>
             <ul className={style.box}>
-                <li className={style.topRatedItem}></li>
-                <li className={style.topRatedItem}></li>
-                <li className={style.topRatedItem}></li>
-                <li className={style.topRatedItem}></li>
+                <li className={style.topRatedItem}>
+                    <img alt="" src="books-cover/rokkaNoYuusha.jpg" className={style.booksCovers}></img>
+                    <h5>Герои шести цветов</h5>
+                </li>
+                <li className={style.topRatedItem}>
+                    <img alt="" src="books-cover/noGameNoLife.jpg" className={style.booksCovers}></img>
+                    <h5>Нет игры нет жизни!</h5>
+                </li>
+                <li className={style.topRatedItem}>
+                    <img alt="" src="books-cover/reZero.jpg" className={style.booksCovers}></img>
+                    <h5>Re:Zero. Жизнь с нуля в альтернативном мире</h5>
+                </li>
+                <li className={style.topRatedItem}>
+                    <img alt="" src="books-cover/spiceAndWolf.webp" className={style.booksCovers}></img>
+                    <h5>Волчица и пряности</h5>
+                </li>
             </ul>
         </div>
     )
